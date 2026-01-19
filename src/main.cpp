e21cb09e761fa013cedb2074c6ede7595724d4b2
Oleki <olekignaczak78@gmail.com>
2026-01-19 13:51:23 +0100

    drawing FloatRect by converting into RectangleSHape

--- src/main.cpp
+++ src/main.cpp
@@ -98,15 +98,33 @@ int main()
         window.draw(shape2);
 
         sf::CircleShape shape3(50.f);
-        shape3.setFillColor(sf::Color(100, 250, 50));
+        shape3.setPosition({30, 20});
+        shape3.setFillColor(sf::Color(100, 50, 50));
         window.draw(shape3);
+
+        sf::FloatRect shape4({25, 100}, {75, 150});
+        //window.draw(shape4);  - zle, trzeba miec RectangleShape
+        sf::RectangleShape drawableRect;
+        drawableRect.setSize({shape4.size.x, shape4.size.y});
+        drawableRect.setPosition({shape4.position.x, shape4.position.y});
+        drawableRect.setFillColor(sf::Color::Magenta);
+        window.draw(drawableRect);
+
+        //TODO: klasy pokroju FloatRect, IntRect mają metody z wykrywaniem kolizji dla innych prostokątów.
+        //TODO: Klasy typu CircleShape, RectangleShape służą jedynie do renderu, nie mają wykrywania kolizji
+        //TODO: Trzeba więc samemu dopisać kolizje albo użyć czego innego :)
 
         float rrW = 150;
         float rrH = 150;
-        sf::RoundedRectangleShape roundedRect({rrW,rrH}, 10,10);
+        sf::RoundedRectangleShape roundedRect({rrW,rrH}, 15,10);
         roundedRect.setPosition({vwX/2 - rrW/2, vwY/2 - rrH/2});
         roundedRect.setFillColor(sf::Color::Red);
         window.draw(roundedRect);
+
+        //TODO: moze sie przydac do kolizji - zwraca rect
+        sf::FloatRect bounds = roundedRect.getGlobalBounds();
+        std::cout << "size: " << "x: " << bounds.size.x << " y: " << bounds.size.y << "\n";
+        std::cout << "position: " << "x: " << bounds.position.x << " y: " << bounds.position.y << "\n";
 
         window.display();
     }
